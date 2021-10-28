f08277a48911e1f61f3cfe6101b06c34ee4c2be9
Bishnu Pandey <chitwan2018@gmail.com>
2021-10-28 19:00:30 -0400

    'added'

---
+++ src/main/java/com/example/laptopList/MyLaptopImplementation.java
@@ -0,0 +1,7 @@
+
+public class MyLaptopImplementation {
+    public static void main(String[] args) {
+        MyLaptopShopping myLaptopShopping = new MyLaptopShopping();
+        myLaptopShopping.getChoice();
+    }
+}
